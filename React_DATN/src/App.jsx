0eb83c4d150e96f55f850017d904716146d2d821
Lê Văn Võ <vole543215@gmail.com>
2023-09-07 22:16:02 +0700

    Khung FE react + base HTML

--- React_DATN/src/App.jsx
+++ React_DATN/src/App.jsx
@@ -1,26 +1,20 @@
 import { Routes, Route } from "react-router-dom"
-// import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import Layout_Web from './Page/Layout/Layout_Web'
 import Contact from "./Page/Contact"
 import Bill from "./Page/Bill"
 import Cart from "./Page/Cart"
-import HomePage from "./Page"
-import Login from "./Page/login"
-import Register from "./Page/register"
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Layout_Web />}>
-        <Route index element={<HomePage />} />
+        <Route index element={<h1>HOME-PAGE</h1>} />
         <Route path="cart" element={<Cart />} />
         <Route path="bill" element={<Bill />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-
       </Route>
     </Routes>
   )
