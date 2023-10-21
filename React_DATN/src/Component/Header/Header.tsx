153d5c616bbee979df8baed8099771a8d5f257ef
quy145 <quynb2003@gmail.com>
2023-10-21 23:24:56 +0700

    update css

--- React_DATN/src/Component/Header/Header.tsx
+++ React_DATN/src/Component/Header/Header.tsx
@@ -41,241 +41,100 @@ const Header = ({ onSearch }) => {
   return (
     <header>
       {contexHolder}
-      <div className="top-link">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-7 offset-lg-3 col-md-9 d-none d-md-block">
-              <div className="site-option">
-                <ul>
-                  <li className="currency">
-                    <a href="#">
-                      USD <i className="fa fa-angle-down" />{" "}
-                    </a>
-                    <ul className="sub-site-option">
-                      <li>
-                        <a href="#">Eur</a>
-                      </li>
-                      <li>
-                        <a href="#">Usd</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-              <div className="call-support">
-                <p>
-                  Call support free: <span> (800) 123 456 789</span>
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-3 position-relative">
-              <div className="dashboard">
-                {/* Thanh tìm kiếm */}
-                <div className="account-menu py-4 relative">
-                  <input type="checkbox" hidden id='search-webSite' />
-                  <label htmlFor="search-webSite"><img className='w-10 mt-2 active:scale-90 cursor-pointer' src="../../../img/search-main-web.png" alt="" /></label>
-                  <form className='form-webSite' action="" onSubmit={handleSearch}>
-                    <label htmlFor="search-webSite" className=' float-right'><ImCancelCircle className="w-5 h-5 m-1 hover:rotate-90 duration-200 cursor-pointer" /></label>
-                    <h1 className='text-center text-2xl mt-3 text-gray-500 font-bold'>Tìm Kiếm</h1>
-                    <input type="text" placeholder=' Bạn đang tìm kiếm gì ?' value={searchKeyword}
-                      onChange={(e) => setSearchKeyword(e.target.value)} />
-                  </form>
-                  <label htmlFor="search-webSite" className='display-website-search'></label>
-                </div>
-                {user ? (<div className="account-menu">
-                  <span>{user.username}</span>
-                  <button onClick={handleLogout}>Logout</button>
-                </div>) : (
-                  <div className="account-menu">
-                    <Link to={`/login`}><UserOutlined style={{ fontSize: "20px", color: "black" }} /></Link>
-                  </div>
-                )
-                }
-
-                <div className="cart-menu">
-                  <ul>
-                    <li>
-                      <a href="#">
-                        {" "}
-                        <img src="img/icon-cart.png" alt="" /> <span>2</span>{" "}
-                      </a>
-                      <div className="cart-info">
-                        <ul>
-                          <li>
-                            <div className="cart-img">
-                              <img src="img/cart/1.png" alt="" />
-                            </div>
-                            <div className="cart-details">
-                              <a href="#">Fusce aliquam</a>
-                              <p>1 x $174.00</p>
-                            </div>
-                            <div className="btn-edit" />
-                            <div className="btn-remove" />
-                          </li>
-                          <li>
-                            <div className="cart-img">
-                              <img src="img/cart/2.png" alt="" />
-                            </div>
-                            <div className="cart-details">
-                              <a href="#">Fusce aliquam</a>
-                              <p>1 x $777.00</p>
-                            </div>
-                            <div className="btn-edit" />
-                            <div className="btn-remove" />
-                          </li>
-                        </ul>
-                        <h3>
-                          Subtotal: <span> $951.00</span>
-                        </h3>
-                        <a href="checkout.html" className="checkout">
-                          checkout
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="mainmenu-area product-items">
         <div className="container">
           <div className="row">
             <div className="col-lg-3">
               <div className="logo">
                 <a href="index.html">
-                  <img src="img/logo.png" alt="" />
+                  <Link to={`/`}><img src="../../../img/Logo_DATN.png" alt="" /></Link >
                 </a>
               </div>
             </div>
-            <div className="col-lg-9">
+            <div className="col-lg-6">
               <div className="mainmenu">
                 <nav>
                   <ul>
                     <li>
                       <a href="/">Home</a>
-
                     </li>
-
                     <li>
                       <a href="/products">Sản phẩm</a>
                     </li>
-
-                    {/* <li className="mega-women">
-                        <a href="shop.html">Women</a>
-                        <div className="mega-menu women">
-                          <div className="part-1">
-                            <span>
-                              <a href="#">Dresses</a>
-                              <a href="#">Cocktail</a>
-                              <a href="#">Day</a>
-                              <a href="#">Evening</a>
-                              <a href="#">Sports</a>
-                            </span>
-                            <span>
-                              <a href="#">shoes</a>
-                              <a href="#">Sports</a>
-                              <a href="#">run</a>
-                              <a href="#">sandals</a>
-                              <a href="#">Books</a>
-                            </span>
-                            <span>
-                              <a href="#">Handbags</a>
-                              <a href="#">Blazers</a>
-                              <a href="#">table</a>
-                              <a href="#">coats</a>
-                              <a href="#">kids</a>
-                            </span>
-                            <span>
-                              <a href="#">Clothing</a>
-                              <a href="#">T-shirts</a>
-                              <a href="#">coats</a>
-                              <a href="#">Jackets</a>
-                              <a href="#">jeans</a>
-                            </span>
-                          </div>
-                          <div className="part-2">
-                            <a href="#">
-                              <img src="img/banner/menu-banner.png" alt="" />
-                            </a>
-                          </div>
-                        </div>
-                      </li> */}
-
-                    {/* <li className="mega-men">
-                        <a href="shop.html">Men</a>
-                        <div className="mega-menu men">
-                          <span>
-                            <a href="#">Bags</a>
-                            <a href="#">Bootees Bags</a>
-                            <a href="#">Blazers</a>
-                          </span>
-                          <span>
-                            <a href="#">Clothing</a>
-                            <a href="#">coats</a>
-                            <a href="#">T-shirts</a>
-                          </span>
-                          <span>
-                            <a href="#">Lingerie</a>
-                            <a href="#">Bands</a>
-                            <a href="#">Furniture</a>
-                          </span>
-                        </div>
-                      </li> */}
-
-                    <li className="mega-jewellery">
-                      <a href="shop.html">Jewellery</a>
+                    <li >
+                      <a href="/blog">Blog</a>
                     </li>
                     <li>
-                      <a href="shop.html">accessories</a>
-                    </li>
-                    <li>
-                      <a href="#">Pages</a>
-                      <div className="sub-menu pages">
-                        <span>
-                          <a href="about-us.html">About us</a>
-                        </span>
-                        <span>
-                          <a href="blog.html">Blog</a>
-                        </span>
-                        <span>
-                          <a href="blog-details.html">Blog Details</a>
-                        </span>
-                        <span>
-                          <a href="cart.html">Cart</a>
-                        </span>
-                        <span>
-                          <a href="checkout.html">Checkout</a>
-                        </span>
-                        <span>
-                          <a href="contact.html">Contact</a>
-                        </span>
-                        <span>
-                          <a href="my-account.html">My account</a>
-                        </span>
-                        <span>
-                          <a href="shop.html">Shop</a>
-                        </span>
-                        <span>
-                          <a href="shop-list.html">Shop list</a>
-                        </span>
-                        <span>
-                          <a href="single-product.html">Single Shop</a>
-                        </span>
-                        <span>
-                          <a href="login.html">Login page</a>
-                        </span>
-                        <span>
-                          <a href="register.html">Ragister page</a>
-                        </span>
-                        <span>
-                          <a href="wishlist.html">Wishlist</a>
-                        </span>
-                      </div>
+                      <a href="/contact">Contact</a>
                     </li>
                   </ul>
                 </nav>
+              </div>
+            </div>
+            <div className="col-lg-3 dashboard">
+              {/* Thanh tìm kiếm */}
+              <div className="searchBar">
+                <input type="checkbox" hidden id='search-webSite' />
+                <label htmlFor="search-webSite"><img className='w-8 active:scale-90 cursor-pointer' src="../../../img/search-main-web.png" alt="" /></label>
+                <form className='form-webSite' action="" onSubmit={handleSearch}>
+                  <label htmlFor="search-webSite" className=' float-right'><ImCancelCircle className="w-5 h-5 m-1 hover:rotate-90 duration-200 cursor-pointer" /></label>
+                  <h1 className='text-center text-2xl mt-3 text-gray-500 font-bold'>Tìm Kiếm</h1>
+                  <input type="text" placeholder=' Bạn đang tìm kiếm gì ?' value={searchKeyword}
+                    onChange={(e) => setSearchKeyword(e.target.value)} />
+                </form>
+                <label htmlFor="search-webSite" className='display-website-search'></label>
+              </div>
+              {user ? (<div className="account-menu">
+                <span>{user.username}</span>
+                <button onClick={handleLogout}>Logout</button>
+              </div>) : (
+                <div className="account-menu">
+                  <Link to={`/login`}><UserOutlined style={{ fontSize: "30px", color: "#232323" }} /></Link>
+                </div>
+              )
+              }
+
+              <div className="cart-menu">
+                <ul>
+                  <li>
+                    <a href="#">
+                      {" "}
+                      <img src="img/icon-cart.png" style={{ width: "23px" }} alt="" /> <span>2</span>{" "}
+                    </a>
+                    <div className="cart-info">
+                      <ul>
+                        <li>
+                          <div className="cart-img">
+                            <img src="img/cart/1.png" alt="" />
+                          </div>
+                          <div className="cart-details">
+                            <a href="#">Fusce aliquam</a>
+                            <p>1 x $174.00</p>
+                          </div>
+                          <div className="btn-edit" />
+                          <div className="btn-remove" />
+                        </li>
+                        <li>
+                          <div className="cart-img">
+                            <img src="img/cart/2.png" alt="" />
+                          </div>
+                          <div className="cart-details">
+                            <a href="#">Fusce aliquam</a>
+                            <p>1 x $777.00</p>
+                          </div>
+                          <div className="btn-edit" />
+                          <div className="btn-remove" />
+                        </li>
+                      </ul>
+                      <h3>
+                        Subtotal: <span> $951.00</span>
+                      </h3>
+                      <a href="checkout.html" className="checkout">
+                        checkout
+                      </a>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
             <div className="col-sm-12">
