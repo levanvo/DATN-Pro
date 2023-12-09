9fbd24c5f69e0ac8d558602d364e0a5ec8bbd2ff
vanvo2k <vole543215@gmail.com>
2023-12-09 21:26:08 +0700

    thống kê lại lượt xem

--- React_DATN/src/Page/Admin/Dashboard.tsx
+++ React_DATN/src/Page/Admin/Dashboard.tsx
@@ -2,133 +2,137 @@ import React, { useState, useEffect } from 'react'
 import Highcharts from 'highcharts';
 import { useGetAllProductQuery } from '../../Services/Api_Product';
 import Loading from '../../Component/Loading';
+import { Table } from 'antd';
 
 const Dashboard = () => {
   const { data: dataGetViews, isLoading: loadingViews }: any = useGetAllProductQuery();
-  const [totalViews, setTotalViews]: any = useState(); // default views
   const [totalAllViews, setTotalAllViews]: any = useState(0);
-  const [selectView, setSelectView]: any = useState("top10");
-  const HandelViews = (): void => {
-    Highcharts.chart({
-      chart: {
-        type: 'pie',
-        renderTo: 'StatictisViews'
-      },
-      plotOptions: {
-        pie: {
-          dataLabels: {
-            format: '{point.name}: {point.percentage:.1f} %'
-          },
-        }
-      },
-      tooltip: {
-        pointFormat: '{point.name}: <b>{point.y} lượt xem</b>'
-    },
-      series: totalViews.series,
-    });
-
-  }
-  setTimeout(() => {
-    HandelViews();
-  }, 100);
-
-  const Top10_View = () => {
-    function getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
-    setSelectView("top10");
-    if (!loadingViews) {
-      let arrayNew: any = [];
-      let countViews = 0;
-      dataGetViews.map((views: any) => {
-        if (views.views > 0) {
-          countViews += views.views
-          arrayNew.push({
-            name: views.name,
-            y: views.views,
-            color: getRandomColor()
-          });
-        };
-      });
-      setTotalAllViews(countViews);
-      let data: any = arrayNew.sort((a: any, b: any) => a.y - b.y)
-      data = data.slice(-10)
-      setTotalViews({
-        series: [{
-          data
-        }]
-      });
-    };
+  const [selectView, setSelectView]: any = useState("today");
+  const [viewToday, setViewsToday]: any = useState([]);
+  const [viewWeek, setViewsWeek]: any = useState([]);
+  const [viewMonth, setViewsMonth]: any = useState([]);
+  
+  
+  function TodayViews(){
+    setSelectView("today");
   }
 
-  const Top5_View = () => {
-    setSelectView("top5");
-    const data = totalViews.series[0].data.slice(-5)
-    setTotalViews({
-      series: [{
-        data
-      }]
-    });
+  function WeekViews(){
+    setSelectView("week");
   }
 
+  function MonthViews(){
+    setSelectView("month");
+  }
   useEffect(() => {
-    function getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
     if (!loadingViews) {
-      let arrayNew: any = [];
-      let countViews = 0;
+      let arrayToday: any = [];
+      let arrayWeek: any = [];
+      let arrayMonth: any = [];
+      let countViews=0;
+      const Time=new Date();
+
+      const month=Time.getMonth()+1;
+      const day=Time.getDate();
+      const hours=Time.getHours();
+
       dataGetViews.map((views: any) => {
-        if (views.views > 0) {
-          countViews += views.views
-          arrayNew.push({
-            name: views.name,
-            y: views.views,
-            color: getRandomColor()
-          });
+        views.views>0 ? countViews+=views.views:"";
+
+        let timeSP=new Date(views.createdAt);
+        const daySP:number=timeSP.getDate();
+        if(daySP==day){
+          arrayToday.push(views);
         };
+        if((day-timeSP.getDate())<=7){
+          arrayWeek.push(views);
+        };
+        if((day-timeSP.getDate())<=30){
+          arrayMonth.push(views);
+        };
+        // arrayMonth.push(views);
       });
+      
+      setViewsMonth(arrayMonth);
+      setViewsWeek(arrayWeek);
+      setViewsToday(arrayToday);
       setTotalAllViews(countViews);
-      let data: any = arrayNew.sort((a: any, b: any) => a.y - b.y)
-      data = data.slice(-10)
-      setTotalViews({
-        series: [{
-          data
-        }]
-      });
     };
-  }, [dataGetViews])
+  }, [dataGetViews]);
 
-
+  const columnsViews: any = [
+    {
+      title: 'Tên sản phẩm',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text:any) => <a>{text}</a>,
+    },
+    {
+      title: 'Lượt xem',
+      dataIndex: 'views',
+      key: 'views',
+    }
+  ];
+  
+  let dataViews:any=[];
+  if(selectView=='today'){
+    viewToday?.map((items:any)=>{
+      dataViews.push({
+        key:items._id,
+        name:items.name,
+        views:items.views
+      });
+    });
+  }else if(selectView=="week"){
+    viewWeek?.map((items:any)=>{
+      dataViews.push({
+        key:items._id,
+        name:items.name,
+        views:items.views
+      });
+    });
+  }else{
+    viewMonth?.map((items:any)=>{
+      dataViews.push({
+        key:items._id,
+        name:items.name,
+        views:items.views
+      });
+    });
+    
+  };
+  dataViews.sort((a: any, b: any) => b.views-a.views)
   return (
     <div className='h-[80vh] scrollDasboard'>
-
       <div className="flex justify-between border rounded-md VIEWS select-none">
         <div className={`selector-Views`}>
           <h3 className='mt-2 text-sky-600 ml-10'>Thống kê theo lượt xem:</h3>
-          <p className='ml-2'>(*) Mặc định 10 sản phẩm có lượt views cao nhất</p>
-          <p className='-mt-4 ml-2'>(*) Tổng lượt xem toàn bộ: <span className='text-orange-500'>{totalAllViews} views /{dataGetViews?.length} sản phẩm.</span></p>
+          <p className='ml-2'>(*) <span className='text-red-400'>Lượt xem được phân tích theo khoảng thời gian được tạo của các sản phẩm.</span></p>
+          <p className='-mt-4 ml-2'>(*) Tổng lượt xem toàn bộ: <span className='text-green-600'>{totalAllViews} views /{dataGetViews?.length} sản phẩm.</span></p>
           <div className="flex space-x-2 ml-20 ">
-            {selectView == "top10" ? <input type="radio" id='top10' name='a' checked /> : <input type="radio" id='top10' name='a' disabled/>}
-            <label htmlFor='top10'><p className='m-0 cursor-pointer font-bold' onClick={() => Top10_View()}>Top 10 sản phẩm lượt xem cao nhất. </p></label>
+            {selectView == "today" ? <input type="radio" id='today' name='a' checked/> : <input type="radio" id='today' name='a'/>}
+            <label htmlFor='today'><p className='m-0 cursor-pointer font-bold' onClick={() => TodayViews()}>Lượt xem hôm nay. </p></label>
           </div>
           <div className="flex space-x-2 ml-20 ">
-            {selectView == "top5" ? <input type="radio" id='top5' name='a' checked /> : <input type="radio" id='top5' name='a' disabled/>}
-            <label htmlFor='top5'><p className='m-0 cursor-pointer font-bold' onClick={() => Top5_View()}>Top 5 sản phẩm lượt xem cao nhất. </p></label>
-
+            {selectView == "week" ? <input type="radio" id='week' name='a' checked/> : <input type="radio" id='week' name='a'/>}
+            <label htmlFor='week'><p className='m-0 cursor-pointer font-bold' onClick={() => WeekViews()}>Lượt xem 7 ngày qua. </p></label>
+          </div>
+          <div className="flex space-x-2 ml-20 ">
+            {selectView == "month" ? <input type="radio" id='month' name='a' checked/> : <input type="radio" id='month' name='a'/>}
+            <label htmlFor='month'><p className='m-0 cursor-pointer font-bold' onClick={() => MonthViews()}>Lượt xem tháng này. </p></label>
           </div>
         </div>
-        {loadingViews ? <Loading /> : <div className='w-[900px] h-[500px]' id={`StatictisViews`}></div>}
+        <div className="">
+          <div className="flex space-x-2 -mb-10 mt-5 bg-gray-50 rounded-full w-fit">
+            <img className='w-7 h-7' src="../../../img/icons/timetable.png" alt="" />
+            {selectView=="today" && <p className='pr-5 mt-1 h-[12px]'>hôm nay: {(new Date().getDate())<10?"0"+(new Date().getDate()):(new Date().getDate())}/{(new Date().getMonth()+1)>12?"01":(new Date().getMonth()+1)}/{new Date().getFullYear()}</p>}
+            {selectView=="week" && <p className='pr-5 mt-1 h-[12px]'>từ: 7 ngày trước</p>}
+            {selectView=="month" && <p className='pr-5 mt-1 h-[12px]'>từ: {(new Date().getDate())<10?"0"+(new Date().getDate()):(new Date().getDate())}/{(new Date().getMonth()+1)>12?"01":(new Date().getMonth())}/{new Date().getFullYear()} đến: {(new Date().getDate())<10?"0"+(new Date().getDate()):(new Date().getDate())}/{(new Date().getMonth()+1)>12?"01":(new Date().getMonth()+1)}/{new Date().getFullYear()}</p>}
+            
+          </div>
+          <p className='float-right mr-5'>số lượng SP: {selectView=="today" && viewToday.length} {selectView=="week" && viewWeek.length} {selectView=="month" && viewMonth.length}</p>
+          {loadingViews ? <Loading /> : <Table className='w-[700px] mt-5 mr-2' columns={columnsViews} dataSource={dataViews} pagination={{pageSize:5}}/>}
+        </div>
 
       </div>
 
