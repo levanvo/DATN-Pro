b70a8d75432136aaf4ddbe15d472219ff9ac9311
thuanph25425 <tranthuan140623@gmail.com>
2023-12-17 16:46:40 +0700

    update bill admin v3

--- React_DATN/src/Page/Admin/Bill/BillList.tsx
+++ React_DATN/src/Page/Admin/Bill/BillList.tsx
@@ -7,17 +7,20 @@ import { Link } from 'react-router-dom';
 import { Option } from 'antd/es/mentions';
 import moment from 'moment';
 import Input from 'antd/es/input/Input';
+import { deburr } from 'lodash';
 
 const BillList = () => {
   const { data, isLoading, error } = useGetAllOrdersQuery(undefined);
 
   const [filterStatus, setFilterStatus] = useState('');
 
+  const [filterName, setFilterName] = useState('');
+
   const dataSource = data?.map((order: IOrder) => ({
     key: order._id,
     code_order: order?.code_order,
     userId: order?.userId?.username || "Khách hàng",
-    createdAt: moment(order?.createdAt).format('DD-MM-YYYY | HH:mm'),
+    createdAt: moment(order?.createdAt).format('HH:mm | DD-MM-YYYY'),
     status: order?.status,
   }));
 
@@ -35,9 +38,14 @@ const BillList = () => {
   const [filterCode, setFilterCode] = useState('');
 
   const handleCodeFilter = (e) => {
-    setFilterCode(e.target?.value);
+    const filterValue = e.target?.value;
+    setFilterCode(filterValue);
   };
 
+  const handleNameFilter = (e) => {
+    const filterValue = deburr(e.target?.value); // Loại bỏ các dấu trong chuỗi
+    setFilterName(filterValue);
+  };
 
   const columns = [
     {
@@ -88,34 +96,49 @@ const BillList = () => {
 
   const filteredData = dataSource?.filter((order: IOrder) => {
     const matchStatus = !filterStatus || order.status === filterStatus;
-    const matchCode = !filterCode || order.code_order.includes(filterCode);
-    return matchStatus && matchCode;
+    const matchCode = !filterCode || order.code_order.toLowerCase().includes(filterCode.toLowerCase());
+    const matchName = !filterName || deburr(order.userId).toLowerCase().includes(deburr(filterName).toLowerCase());
+    return matchStatus && matchCode && matchName;
   });
 
   return (
     <div>
       <Divider />
 
-      <div style={{ marginBottom: '16px' }}>
-      <span style={{ marginRight: '8px' }}>Tìm theo mã đơn hàng:</span>
-        <Input
-          value={filterCode}
-          onChange={handleCodeFilter}
-          style={{ width: 150 }}
-        />
-        <span style={{ marginRight: '8px', marginLeft: 30 }}>Lọc theo trạng thái:</span>
-        <Select
-          value={filterStatus}
-          onChange={handleStatusFilter}
-          style={{ width: 150 }}
-        >
-          <Option value="">Tất cả</Option>
-          <Option value="0">Đang chờ xác nhận</Option>
-          <Option value="1">Đã xác nhận</Option>
-          <Option value="2">Đã hủy</Option>
-          <Option value="3">Đang giao hàng</Option>
-          <Option value="4">Đã nhận hàng</Option>
-        </Select>
+      <div style={{ marginBottom: '16px', display: 'flex' }}>
+        <div>
+          <span style={{ marginRight: '8px' }}>Tìm theo mã đơn hàng:</span>
+          <Input
+            value={filterCode}
+            onChange={handleCodeFilter}
+            style={{ width: 150 }}
+          />
+        </div>
+
+        <div>
+          <span style={{ marginRight: '8px', marginLeft: 50 }}>Tìm theo tên khách hàng:</span>
+          <Input
+            value={filterName}
+            onChange={handleNameFilter}
+            style={{ width: 150 }}
+          />
+        </div>
+
+        <div>
+          <span style={{ marginRight: '8px', marginLeft: 50 }}>Lọc theo trạng thái:</span>
+          <Select
+            value={filterStatus}
+            onChange={handleStatusFilter}
+            style={{ width: 150 }}
+          >
+            <Option value="">Tất cả</Option>
+            <Option value="0">Đang chờ xác nhận</Option>
+            <Option value="1">Đã xác nhận</Option>
+            <Option value="2">Đã hủy</Option>
+            <Option value="3">Đang giao hàng</Option>
+            <Option value="4">Đã nhận hàng</Option>
+          </Select>
+        </div>
       </div>
 
       {isLoading ? (
